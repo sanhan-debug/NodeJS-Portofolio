5ec96e235759cc83b141b0c67b4962c48be744cd
sanhan-debug <snhansayilov@gmail.com>
2024-09-01 15:14:15 +0400

    v2

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ const URI = process.env.URI
 app.use(express.static('public'))
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
-app.set("view engine", "ejs")
+app.set("view engine","ejs")
 
 
 
