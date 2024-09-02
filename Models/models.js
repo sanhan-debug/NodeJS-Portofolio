484981b864b94e053b768fff61c1f1a79dabe270
sanhan-debug <snhansayilov@gmail.com>
2024-09-02 15:54:49 +0400

    v3

--- Models/models.js
+++ Models/models.js
@@ -2,29 +2,30 @@ import { model, Schema } from "mongoose";
 
 
 const userSchema = new Schema({
-    username:String,
-    surname:String,
-    email:String,
-    password:String,
-    job:String,
-    description:String,
-    photo:String,
-},{
-    versionKey:false,
-    timestamps:true
+    username: String,
+    surname: String,
+    email: String,
+    password: String,
+    job: String,
+    description: String,
+    photo: String,
+}, {
+    versionKey: false,
+    timestamps: true
 })
 
 const projectSchema = new Schema({
-    projectname:String,
-    user:{
-        type:Schema.Types.ObjectId,
-        ref:"users",
-    }
-},{
-    versionKey:false,
-    timestamps:true
+    projectname: String,
+    userId: {
+        type: Schema.Types.ObjectId,
+        ref: "users",
+    },
+    description: String,
+}, {
+    versionKey: false,
+    timestamps: true
 })
 
 
-export const userModel = model("users",userSchema)
-export const projectModel = model("projects",projectSchema)
+export const userModel = model("users", userSchema)
+export const projectModel = model("projects", projectSchema)
--- app.js
+++ app.js
@@ -4,16 +4,20 @@ import path from 'path';
 import { connect } from 'mongoose';
 import { projectModel, userModel } from './Models/models.js';
 import { customizedMulter } from './multer.js';
+import bcrypt from 'bcrypt'
+import jwt from 'jsonwebtoken'
+
 
 
 const app = express();
 dotenv.config()
 const PORT = process.env.PORT
 const URI = process.env.URI
+const SECRET_KEY = process.env.SECRET_KEY
 app.use(express.static('public'))
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
-app.set("view engine","ejs")
+app.set("view engine", "ejs")
 
 
 
@@ -33,24 +37,49 @@ app.get('/new-project', (req, res) => {
     res.sendFile(path.resolve('./views/work.html'))
 })
 
-app.post('/register', customizedMulter.single("avatar"), (req, res) => {
-    const { username, surname, email, job, password, description } = req.body
-    const { filename } = req.file
+app.post('/register', customizedMulter.single("avatar"), async (req, res) => {
+    try {
+        const { username, surname, email, job, password, description } = req.body;
+        const { filename } = req.file;
+        const hashingPass = bcrypt.hashSync(password, 10)
 
-    const user = userModel.create({
-        username,
-        surname,
-        email,
-        job,
-        description,
-        password,
-        photo: `/${filename}`
-    })
+        const user = await userModel.create({
+            username,
+            surname,
+            email,
+            job,
+            description,
+            password: hashingPass,
+            photo: `/${filename}`
+        });
 
-    res.render('index',{user})
+        res.send(user)
+
+    } catch (error) {
+        console.error(error);
+        res.status(500).send("Server Error");
+    }
+});
+
+
+app.post('/login', async (req, res) => {
+    const { username, password } = req.body
+    const user = await userModel.findOne({ username })
+
+    if (user) {
+        const match = await bcrypt.compare(password, user.password)
+        if (match) {
+            res.send("sucess")
+        } else {
+            res.send("password is incorrect")
+        }
+    } else {
+        res.send("user is not defined")
+    }
+
 })
 
-app.post('/new-project',(req,res)=>{
+app.post('/new-project', (req, res) => {
     const project = projectModel.create
 })
 
